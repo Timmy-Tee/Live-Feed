0a0f04bb404ce36dded4b55bab6ea8fbd13d6496
Egbetokun Timilehin <timmyaof02@gmail.com>
2024-01-07 05:19:56 +0100

    finished the features

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
       "main-color": "#0A40C0",
       "text-color": "#667185",
       "caption-color": "#1E1E1E",
-      "border-color": "#EAECF0"
+      "border-color": "#EAECF0",
+      "paragraph": "#667085",
+      "icon-bg": "#DAE2F6"
+    },
+    height: {
+      100: "100vh",
+      380: '380px',
+      400: '400px'
+    },
+    lineHeight: {
+      70: "70px",
+      24: "24px"
     }
   },
   plugins: [],
