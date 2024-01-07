4676f1f4a9aacf206a5b3f18ff147cd6524033f4
Egbetokun Timilehin <timmyaof02@gmail.com>
2024-01-07 02:34:00 +0100

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {},
+
+    colors:{
+      "main-color": "#0A40C0",
+      "text-color": "#667185",
+      "caption-color": "#1E1E1E",
+      "border-color": "#EAECF0"
+    }
+  },
+  plugins: [],
+}
